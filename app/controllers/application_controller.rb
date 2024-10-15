5db29d24f1ab8af4c9f0569dded1b5f9f777e10d
star_ <star_berry2s@yahoo.co.jp>
2024-10-15 14:42:08 +0900

    ユーザー認証まわり

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,21 @@
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name avatar])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name avatar])
+  end
+
+  def after_sign_in_path_for(_resource_or_scope)
+    root_path
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    root_path
+  end
+  
   allow_browser versions: :modern
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,3 +1,11 @@
 class StaticPagesController < ApplicationController
+  skip_before_action :authenticate_user!
+
   def top; end
+
+  def privacy_policy; end
+
+  def terms_of_service; end
+
+  def contact_us; end
 end
