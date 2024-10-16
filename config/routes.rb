39774eea6d7351f6bb966535b3f9b6806c1927fe
star_ <star_berry2s@yahoo.co.jp>
2024-10-16 11:43:32 +0900

    フラッシュメッセージ設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
     sessions: 'users/sessions',
+    registrations: 'registrations',
     registrations: 'users/registrations',
   }
 
